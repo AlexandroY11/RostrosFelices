1d4804ff850bf5abdf3447134b87aba27b2b3d5e
JoanWolf <dasstian@hotmail.com>
2023-11-18 12:42:33 -0500

    Client Model

---
+++ Models/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace RostrosFelices.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+    }
+}
