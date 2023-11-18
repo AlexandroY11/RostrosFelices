a169d5a4e8a79b7a346e12e30401359dce48830f
JoanWolf <dasstian@hotmail.com>
2023-11-18 13:11:17 -0500

    Services

---
+++ Models/Services.cs
@@ -0,0 +1,10 @@
+﻿namespace RostrosFelices.Models
+{
+    public class Services
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } //Tipo de servicio
+        public string Description { get; set; } // Tipo de servicio estetico requerido 
+      
+    }
+}
