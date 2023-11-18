db7dc1368ca82521c0e56d8c6fc8de2eff070ebc
AlexandroY11 <alexandroyule@gmail.com>
2023-11-18 14:33:56 -0500

    Service Model Update II

--- Models/Service.cs
+++ Models/Service.cs
@@ -1,16 +1,26 @@
-﻿namespace RostrosFelices.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RostrosFelices.Models
 {
     public class Service
     {
         public int Id { get; set; }
         public string Name { get; set; } //Tipo de servicio
         public DateTime Fecha { get; set; }
-        public int EmployeeId { get; set; }
-        public string EmployeeName { get; set; }  
-        public int ClientId { get; set; }  
-        public string ClientName { get; set; }  
-        public string Observation { get; set; }  
 
-          
+        [ForeignKey("Employee")]
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+
+        [ForeignKey("Client")]
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+
+        public string Observation { get; set; }
+
+        public Employee Employee { get; set; }
+
+        public Client Client { get; set; }
     }
 }
