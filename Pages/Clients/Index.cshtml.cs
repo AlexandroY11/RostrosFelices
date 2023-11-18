3ea3284e71dcb11a96deaa69a323c61e38afb1f5
AlexandroY11 <alexandroyule@gmail.com>
2023-11-18 13:30:53 -0500

    Index Client

---
+++ Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RostrosFelices.Data;
+using RostrosFelices.Models;
+
+namespace RostrosFelices.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RostrosFelicesContext _context;
+
+        public IndexModel(RostrosFelicesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Client> Clients { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if(_context.Clients != null)
+            {
+                Clients = await _context.Clients.ToListAsync();
+            }
+        }
+    }
+}
