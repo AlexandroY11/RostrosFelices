3daaa89437ccefdbcc1d35a95b847ccf2eb8f0fa
AlexandroY11 <alexandroyule@gmail.com>
2023-11-19 15:25:19 -0500

    Authorize

--- Pages/Clients/Index.cshtml.cs
+++ Pages/Clients/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -6,7 +7,9 @@ using RostrosFelices.Models;
 
 namespace RostrosFelices.Pages.Clients
 {
-    public class IndexModel : PageModel
+	[Authorize]
+
+	public class IndexModel : PageModel
     {
         private readonly RostrosFelicesContext _context;
 
